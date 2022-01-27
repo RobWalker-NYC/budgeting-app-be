158d62b0a67e7d4f912d7b5425ad254341195680
rob <robwalker@pursuit.org>
2022-01-26 22:29:49 -0500

    Finished BackEnd

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ https://github.com/RobWalker-NYC/budget-app-be
 https://github.com/RobWalker-NYC/budget-app-fe
 
 ## Deployed Backend Link
- https://rw-backend-budget.herokuapp.com/
+https://rw-backend-budget.herokuapp.com/
 
 ## Deployed Frontend Link
 https://dreamy-raman-d8671a.netlify.app.netlify.app/
--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -2,44 +2,49 @@ const express = require('express');
 const transRoute = express.Router();
 const transArr = require('../models/data.js');
 
+// "/transactions/"
 transRoute.get('/', (req, res) => {
     res.json(transArr)
 });
 
+// "/transactions/:index"
 transRoute.get('/:index', (req, res) => {
-    const { index } = req.params;
-    if(transApp[index]) {
-        res.json(transApp[index])
+    const { index } = req.params
+    if(transArr[index]) {
+        res.json(transArr[index])
     } else {
         res.status(404).json({message: 'Page not found'})
     }
 });
 
+// "/transactions/"
 transRoute.post('/', (req, res) => {
     transArr.push(req.body);
     res.json(transArr[transArr.length - 1]);
 });
 
+// "/transactions/:index"
 transRoute.delete('/:index', (req, res) => {
     const { index } = req.params;
     if (transArr[index]) {
-        const removeTrans = transApp.splice(index, 1);
-        res.json(transApp);
+        const removeTrans = transArr.splice(index, 1);
+        res.json(transArr);
     } else {
         res.status(404).json({message: 'Page not found'});
     }
 });
 
+// "/transactions/:index"
 transRoute.put('/:index', (req, res) => {
-    const { index } = res.params;
-    if(!transApp[index]) {
+    const { index } = req.params;
+    if(!transArr[index]) {
       return res.status(404).json({message: 'Page not found'});
     }
 
     let {date, name, amount, from} = req.body;
     if(date && name && amount && from) {
         transArr[index] = {date, name, amount, from};
-        return res.json(transApp[index]);
+        return res.json(transArr[index]);
     } else {
     res.status(422).json({error: 'Please provide all fields'});
     }
