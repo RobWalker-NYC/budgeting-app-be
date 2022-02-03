6c5e333859986572d9b444175a484fde1c5e6aad
rob <robwalker@pursuit.org>
2022-02-03 17:02:49 -0500

    Update data

--- models/data.js
+++ models/data.js
@@ -1,42 +1,42 @@
 module.exports = [
     {
-      date: "April 1",
+      date: "2022-01-01",
       name: "Groceries",
       amount: -30,
       from: "Ideal"
     },
     {
-      date: "April 1",
+      date: "2022-01-01",
       name: "Shopping",
       amount: -47,
       from: "Target"
     },
     {
-      date: "April 2",
+      date: "2022-01-02",
       name: "Income",
       amount: 2000,
       from: "Google"
     },
     {
-      date: "April 5",
+      date: "2022-01-05",
       name: "Gas",
       amount: -25,
       from: "Shell Gas Station"
     },
     {
-      date: "April 6",
+      date: "2022-01-06",
       name: "Rent",
       amount: -700,
       from: "Pike International, LLC"
     },
     {
-      date: "April 8",
+      date: "2022-01-08",
       name: "Income",
       amount: 2000,
       from: "Google"
     },
     {
-      date: "April 8",
+      date: "2022-01-08",
       name: "Groceries",
       amount: -40,
       from: "Mr. Kiwi"
